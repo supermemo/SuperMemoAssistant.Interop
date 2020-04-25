623281cc5567f9bc1125294f2b0584c0c210a55b
Alexis <alexis@incogito.org>
2020-04-25 22:56:41 +0200

    - Updated: Coding style

--- src/SuperMemoAssistant.Interop/Extensions/AssemblyEx.cs
+++ src/SuperMemoAssistant.Interop/Extensions/AssemblyEx.cs
@@ -6,7 +6,7 @@
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the 
+// and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
 // 
 // The above copyright notice and this permission notice shall be included in
@@ -19,26 +19,22 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
-// 
-// 
-// Created On:   2018/10/26 22:37
-// Modified On:  2019/01/26 06:10
-// Modified By:  Alexis
 
 #endregion
 
 
 
 
-using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-
 namespace SuperMemoAssistant.Extensions
 {
+  using System;
+  using System.Linq;
   using System.Reflection;
+  using System.Runtime.InteropServices;
 
+  /// <summary>
+  /// Extension methods relating to assembly
+  /// </summary>
   public static class AssemblyEx
   {
     #region Methods
@@ -58,7 +54,7 @@ namespace SuperMemoAssistant.Extensions
     public static string GetAssemblyVersion(this Type typeInAssembly)
     {
       var assembly = typeInAssembly.Assembly;
-      var aivAttr = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+      var aivAttr  = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
 
       return aivAttr.InformationalVersion;
     }
